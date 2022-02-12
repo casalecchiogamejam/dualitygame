51cda2a78be1c6d0202abf0d71d6563e0d3e463a
kdpkke <ellolozio93@gmail.com>
2022-02-12 15:14:18 +0100

    converted everything to HDRP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region instance_singleton
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public GameObject bulletPrefab;
+    public GameObject rightHandControllerGO;
+    public GameObject leftHandControllerGO;
+
+    //TODO creare input system per gestione eventi sui tasti dei controller
+}
---
+++ Assets/Scripts/HandManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour
+{
+    public void Shoot()
+    {
+
+    }
+
+}
