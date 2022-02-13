4930fad0a115a2299f34a61ee9419bf30c2246df
kdpkke <ellolozio93@gmail.com>
2022-02-13 17:31:28 +0100

    set correct layers based on the world

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,8 +22,11 @@ public class GameManager : MonoBehaviour
 
     public GenericObjectPooling bulletPool;
     public GenericObjectPooling enemyPool;
+    public GenericObjectPooling soulsPool;
     public PlayerManager player;
     public WorldsManager worldsManager;
+
+    private List<GameObject> allActiveObjs;
 
     private void Awake()
     {
@@ -32,7 +35,27 @@ public class GameManager : MonoBehaviour
             _instance = GetComponent<GameManager>();
         }
 
+        allActiveObjs = new List<GameObject>();
+        CreateAllPoolLists();
+    }
+
+    private void CreateAllPoolLists()
+    {
         bulletPool.CreateNewPoolList();
         enemyPool.CreateNewPoolList();
+        soulsPool.CreateNewPoolList();
+        //initialize all pools here...
+    }
+
+    public List<GameObject> GetAllActiveChildrenOfPool(GenericObjectPooling poolToRetrieve)
+    {
+        allActiveObjs.Clear();
+        foreach (Transform child in poolToRetrieve.gameObject.transform)
+        {
+            if (child.gameObject.activeInHierarchy)
+                allActiveObjs.Add(child.gameObject);
+        }
+
+        return allActiveObjs;
     }
 }
--- Assets/Scripts/WorldsManager.cs
+++ Assets/Scripts/WorldsManager.cs
@@ -9,9 +9,17 @@ public class WorldsManager : MonoBehaviour
     public void ChangeWorld()
     {
         if (currentWorld == Constants.Worlds.RealWorld)
+        {
             currentWorld = Constants.Worlds.SoulsWorld;
+            GameManager.instance.GetAllActiveChildrenOfPool(GameManager.instance.soulsPool).ForEach(soul => soul.layer = LayerMask.NameToLayer("Default"));
+            GameManager.instance.GetAllActiveChildrenOfPool(GameManager.instance.enemyPool).ForEach(enemy => enemy.layer = LayerMask.NameToLayer("No Hit"));
+        }
         else
+        {
             currentWorld = Constants.Worlds.RealWorld;
+            GameManager.instance.GetAllActiveChildrenOfPool(GameManager.instance.soulsPool).ForEach(soul => soul.layer = LayerMask.NameToLayer("No Hit"));
+            GameManager.instance.GetAllActiveChildrenOfPool(GameManager.instance.enemyPool).ForEach(enemy => enemy.layer = LayerMask.NameToLayer("Default"));
+        }
 
         // TODO: change world graphically
     }
