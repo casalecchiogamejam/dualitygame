7252b3b33977bea855845f6cf6d3c11c0998cf16
dtolomelli <daniele.tolomelli@vection.com.au>
2022-02-13 10:31:40 +0100

    Added absorb effect and sphere scale effect

---
+++ Assets/Scripts/SphereEffect.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereEffect : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // teniamo una box volume sempre attiva in scena che rappresenta il mondo attuale
+    // l'animazione di switch scala una box e alla fine dell'animazione va a settare la box attuale con quella dell'animazione corrente
+}
